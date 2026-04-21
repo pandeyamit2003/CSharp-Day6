7fc9c55707c60a8f00497479ca1606af7275d2c7
Amit Pandey <amitpandeymtr2002@gmail.com>
2026-04-21 11:47:10 +0530

    Add project files.

---
+++ CSharp-Day6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DelegateDemo;
+MathOperation operation = delegateDemo.Add;
+Console.WriteLine("Delegate ref is created and currently it is pointing to Add()");
+//invoking the delegate with two integers and displaying the result.
+int result = operation(10, 5);
+Console.WriteLine("Since Delegate is pointing to Add() so the result of Addition is " + result);
+
+//Changing the delegate assignment to the "Subtract" method and invoking it again with the same integers, displaying the result.
+operation = delegateDemo.Subtract;
+Console.WriteLine("Now the delegate ref is changed and currently it is pointing to Subtract()");
+result = operation(10, 5);
+Console.WriteLine("Since Delegate is pointing to Subtract() so the result of Subtraction is " + result);
+
+//Changing the delegate assignment to the "Multiply" method and invoking it again with the same integers, displaying the result.
+operation = delegateDemo.Multiply;
+Console.WriteLine("Now the delegate ref is changed and currently it is pointing to Multiply()");
+result = operation(10, 5);
+Console.WriteLine("Since Delegate is pointing to Multiply() so the result of Multiplication is " + result);
---
+++ CSharp-Day6/delegateDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateDemo
+{
+    delegate int MathOperation(int a, int b);
+    public class delegateDemo
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
